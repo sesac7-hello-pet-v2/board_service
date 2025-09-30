d37b9c1b82aa95d5d00a5dc6d406628013d259ee
namung08 <namung08@gmail.com>
2025-09-30 18:10:02 +0900

    :sparkles: feat: 이미지 저장정보 설정
    Signed-off-by: namung08 <namung08@gmail.com>

--- src/main/java/hello/pet/board_service/entity/PostImage.java
+++ src/main/java/hello/pet/board_service/entity/PostImage.java
@@ -1,4 +1,15 @@
 package hello.pet.board_service.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostImage {
+	private Integer displayOrder;
+	private String s3Key;
 }
