e6081a9e3bf48927e17a06aeeb717ee3af4519bd
namung08 <namung08@gmail.com>
2025-09-30 18:12:21 +0900

    :sparkles: feat: 이미지 저장을 위한 필드 추가
    Signed-off-by: namung08 <namung08@gmail.com>

--- src/main/java/hello/pet/board_service/web/dto/request/PostCreateRequest.java
+++ src/main/java/hello/pet/board_service/web/dto/request/PostCreateRequest.java
@@ -1,5 +1,9 @@
 package hello.pet.board_service.web.dto.request;
 
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -7,6 +11,7 @@ public record PostCreateRequest(
 	@NotNull(message = "로그인을 사용자만 글을 작성할 수 있습니다. <br> 다시 로그인을 진행해 주세요.")
 	Long userId,
 	@NotBlank(message = "글의 내용을 입력해 주세요.")
-	String content
+	String content,
+	List<MultipartFile> file
 ) {
 }
