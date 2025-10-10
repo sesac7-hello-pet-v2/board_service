7180454d11da3b0a3b5bbbcb3c4117ee02b7e1cd
namung08 <namung08@gmail.com>
2025-10-10 11:46:55 +0900

    :zap: perf: OpenFeign 설정 클래스를 명시적 연결
    Signed-off-by: namung08 <namung08@gmail.com>

--- src/main/java/hello/pet/board_service/infrastructure/feign/client/ImageServiceClient.java
+++ src/main/java/hello/pet/board_service/infrastructure/feign/client/ImageServiceClient.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
+import hello.pet.board_service.infrastructure.config.feign.OpenFeignConfig;
 import hello.pet.board_service.infrastructure.feign.dto.response.ImageUploadResponse;
 
 @FeignClient(
 	name = "image-service",
-	url = "${spring.cloud.openfeign.image-service:http://localhost:8088}"
+	url = "${spring.cloud.openfeign.image-service:http://localhost:8088}",
+	configuration = OpenFeignConfig.class
 )
 public interface ImageServiceClient {
 
