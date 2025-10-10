972ceba515d38ed65d114b32b520e8d5acf1a966
namung08 <namung08@gmail.com>
2025-10-11 01:32:55 +0900

    :sparkles: feat: 기존 PostPageRequest.java 삭제 후 기능 수정
    Signed-off-by: namung08 <namung08@gmail.com>

--- src/main/java/hello/pet/board_service/web/dto/request/PostGetRequest.java
+++ src/main/java/hello/pet/board_service/web/dto/request/PostGetRequest.java
@@ -1,21 +1,31 @@
 package hello.pet.board_service.web.dto.request;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Min;
 
-public record PostPageRequest(
+public record PostGetRequest(
 	@Min(
 		value = 1,
 		message = "페이지는 1보다 작을 수 없습니다."
 	)
-	int page,
+	Integer page,
 	@Min(
 		value = 1,
 		message = "데이터의 양은 1보다 작을 수 없습니다."
 	)
-	int size
+	Integer size,
+	@Nullable
+	Long userId
 ) {
-	public PostPageRequest() {
-		this(1, 10);
+	// Compact Constructor를 사용하여 null 값일 때 기본값 설정
+	public PostGetRequest {
+		if (page == null) {
+			page = 1;
+		}
+
+		if (size == null) {
+			size = 10;
+		}
 	}
 
 	public int getPageBase() {
