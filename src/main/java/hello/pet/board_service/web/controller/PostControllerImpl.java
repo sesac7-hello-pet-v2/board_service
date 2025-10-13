ca0070c428cba0af05d7d79df586e30f9beba2e9
namung08 <namung08@gmail.com>
2025-10-13 16:17:41 +0900

    :sparkles: feat: 게시글 수정 엔드포인트 추가
    Signed-off-by: namung08 <namung08@gmail.com>

--- src/main/java/hello/pet/board_service/web/controller/PostControllerImpl.java
+++ src/main/java/hello/pet/board_service/web/controller/PostControllerImpl.java
@@ -7,12 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import hello.pet.board_service.entity.Post;
 import hello.pet.board_service.service.PostService;
 import hello.pet.board_service.web.dto.request.PostCreateRequest;
+import hello.pet.board_service.web.dto.request.PostEditRequest;
 import hello.pet.board_service.web.dto.request.PostGetRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -42,5 +45,13 @@ public class PostControllerImpl implements PostController {
 	public ResponseEntity<Post> getPost(@PathVariable String id) {
 		Post post = service.findOne(id);
 		return ResponseEntity.ok(post);
+	}
+
+	@Override
+	@PutMapping("/{id}")
+	public ResponseEntity<String> editPostContent(@PathVariable String id,
+		@Valid @RequestBody PostEditRequest request) {
+		String editedId = service.editPostContentById(id, request);
+		return ResponseEntity.ok(editedId);
 	}
 }
