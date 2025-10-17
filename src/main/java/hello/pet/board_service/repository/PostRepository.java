7bd6097b1471db6ad63803613f53f5973a87587a
namung08 <namung08@gmail.com>
2025-10-17 16:08:47 +0900

    :zap: perf: 쿼리 필터 추가로 카운트 정확성 보장
    - `addLike`는 userId 가 likeUserIds에 없을 때만 매칭하여 업데이트
    - `removeLike`는 userId가 likeUserIds에 일을 때만 매칭하여 업데이트
    - 반환값 long을 통해 실제로 변경이 발생했는지 확인 가능
    
    Signed-off-by: namung08 <namung08@gmail.com>

--- src/main/java/hello/pet/board_service/repository/PostRepository.java
+++ src/main/java/hello/pet/board_service/repository/PostRepository.java
@@ -11,11 +11,11 @@ import hello.pet.board_service.entity.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 	Page<Post> findByUserId(Long userId, Pageable pageable);
 
-	@Query("{ '_id': ?0 }")
+	@Query("{ '_id': ?0, 'likedUserIds': { '$ne': ?1 } }")
 	@Update("{ '$addToSet': { 'likedUserIds': ?1 }, '$inc': { 'likeCount': 1 } }")
 	long addLike(String postId, Long userId);
 
-	@Query("{ '_id': ?0 }")
+	@Query("{ '_id': ?0, 'likedUserIds': ?1 }")
 	@Update("{ '$pull': { 'likedUserIds': ?1 }, '$inc': { 'likeCount': -1 } }")
 	long removeLike(String postId, Long userId);
 }
